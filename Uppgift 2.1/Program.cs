2b750f8c9bfe7237f97919f336b8ecaefa50571a
alsa07007 <alsa07007@E-PW05CGR5>
2024-10-09 12:48:32 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favorit bok?");
+            String bok = Console.ReadLine();
+            Console.WriteLine("Ok, " + bok + " är din favorit bok!");
+        }
+    }
+}
